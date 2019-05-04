9c61f88ce67fda3b078ee5a8122e21b48a71c976
jeneser <jeneserwang@gmail.com>
2019-05-04 17:57:32 +0800

    Updates

---
+++ precache-manifest.27771a43ce5180bbad10f229b21b2a19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64e64795a61d1f38f740",
+    "url": "/smart-pos/static/js/runtime~main.99644658.js"
+  },
+  {
+    "revision": "1428912bfaf835ad9583",
+    "url": "/smart-pos/static/js/main.1a3b9714.chunk.js"
+  },
+  {
+    "revision": "37abc78ceabb8290da4a",
+    "url": "/smart-pos/static/js/2.00b933ae.chunk.js"
+  },
+  {
+    "revision": "1428912bfaf835ad9583",
+    "url": "/smart-pos/static/css/main.75310be9.chunk.css"
+  },
+  {
+    "revision": "37abc78ceabb8290da4a",
+    "url": "/smart-pos/static/css/2.a978b680.chunk.css"
+  },
+  {
+    "revision": "43a1ea94130d4d7021fb7072aca4b52c",
+    "url": "/smart-pos/index.html"
+  }
+];
